24fbf0df1f44fde3a939b6722ba52284800a8212
crlt18 <caidenreece12@icloud.com>
2025-10-20 15:17:08 +0100

    added blacklist

--- Assets/Scripts/Dictionary.cs
+++ Assets/Scripts/Dictionary.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 public class Dictionary : MonoBehaviour
 {
     public HashSet<string> wordDictionary;
+    public HashSet<string> blacklist;
 
     void Awake()
     {
         wordDictionary = new HashSet<string>();
+        blacklist = new HashSet<string>();
         LoadWords();
     }
 
     private void LoadWords()
     {
+        TextAsset blacklistFile = Resources.Load<TextAsset>("Blacklist");
+        if (blacklistFile == null)
+        {
+            Debug.LogError("Words.txt not found");
+            return;
+        }
+
+        string[] blacklistWords = blacklistFile.text.Split(new char[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
+        foreach (string word in blacklistWords)
+        {
+            string lower = word.ToLower();
+            blacklist.Add(lower);
+        }
+
         TextAsset wordFile = Resources.Load<TextAsset>("Words");
         if (wordFile == null )
         {
@@ -24,7 +40,7 @@ public class Dictionary : MonoBehaviour
         foreach (string word in words)
         {
             string lower = word.ToLower().Trim();
-            if (lower.Length >= 5) //only words 3+ letters
+            if (lower.Length >= 5 && !blacklist.Contains(lower)) //only words 3+ letters
             {
                 wordDictionary.Add(lower);
             }
@@ -32,6 +48,7 @@ public class Dictionary : MonoBehaviour
 
         Debug.Log("Loaded " + wordDictionary.Count + " words");
     }
+
     public bool IsValidWord(string word)
     {
         return wordDictionary.Contains(word.ToLower());
