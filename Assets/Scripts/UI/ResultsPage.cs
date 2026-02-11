824f21c7f994daff3e0e955194a95ce1e277156e
crlt18 <caidenreece12@icloud.com>
2026-02-11 15:20:25 +0000

    score tally

--- Assets/Scripts/UI/ResultsPage.cs
+++ Assets/Scripts/UI/ResultsPage.cs
@@ -5,9 +5,16 @@ using System.Linq;
 public class ResultsPage : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI wordsList;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private int score;
     private void Awake()
     {
-        wordsList.text = string.Join("\n", GameManager.Instance.levelWords.Select(w => $"{w} ({w.Length})"));   //displays the list of words typed with the number of characters in each word
+        foreach (var word in GameManager.Instance.levelWords)
+        {
+            score += (word.Length) * 10;
+        }
+        wordsList.text = string.Join("\n", GameManager.Instance.levelWords.Select(w => $"{w} ({w.Length * 10})"));   //displays the list of words typed with the number of characters in each word
         GameManager.Instance.levelWords.Clear();
+        scoreText.text = "Final Score: " + score.ToString();
     }
 }
